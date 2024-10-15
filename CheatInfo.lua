1756d930769f909ba4c791e75d04afc85555c20e
philipscott1913 <zyonshyper@gmail.com>
2024-10-15 03:22:06 -0700

    Update CheatInfo.lua

--- CheatInfo.lua
+++ CheatInfo.lua
@@ -2,7 +2,7 @@ local Cheats = {}
 
 Cheats["13083893317"] = {
     {"Public", "This is public cheat with no key system", false, "https://api.luarmor.net/files/v3/loaders/6911c12503804c2a0f60d797b61179ef.lua"};
-    {"Private", "This cheat is private and is not open to the public", "https://api.luarmor.net/files/v3/loaders/2fc9214e6fdfc709fa35c8fd932ef973.lua"}
+    {"Private", "This cheat is private and is not open to the public",true, "https://api.luarmor.net/files/v3/loaders/2fc9214e6fdfc709fa35c8fd932ef973.lua"}
 };
 
 return Cheats
