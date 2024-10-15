c77df60c6ad7e9ea0fc7eef8523d58edb882e2ae
philipscott1913 <zyonshyper@gmail.com>
2024-10-15 02:48:58 -0700

    Create CheatInfo.lua

---
+++ CheatInfo.lua
@@ -0,0 +1,8 @@
+local Cheats = {}
+
+Cheats["13083893317"] = {
+    {"Public", "This is public cheat with no key system"};
+    {"Private", "This cheat is private and is not open to the public"}
+};
+
+return Cheats
