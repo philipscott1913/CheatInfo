5ecac88218b19a60c03aa8401ac0beab7cf03a00
philipscott1913 <zyonshyper@gmail.com>
2024-10-16 12:24:39 -0700

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,7 +1,7 @@
 local games = {}
 
 games["13083893317"] = {"Life Sentence", "rbxassetid://136071339893697"};
-games["13997018456"] = {"Operations: Siege", "rbxassetid://108638305697267"};
-games["13997264379"] = {"Operations: Siege", "rbxassetid://108638305697267"};
+games["13997018456"] = {"Operations: Siege", "rbxassetid://138175711835392"};
+games["13997264379"] = {"Operations: Siege", "rbxassetid://138175711835392"};
 
 return games
